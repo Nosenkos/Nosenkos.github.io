2c9b855771c0e0584885adc899c01f9fad8d0bc5
nosenkos <Nosenko2610@mail.ru>
2016-06-04 16:02:54 +0300

    Add the Creative

---
+++ Creative/js/global.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $('.slider1').slick({
+    'autoplay' : true,
+    'prevArrow' : '<a href="#" class="prev-slider arrows"></a>',
+    'nextArrow' : '<a href="#" class="next-slider arrows"></a>',
+  });
+});
